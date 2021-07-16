abd1d0603020fc2eb87f02cf1baf84113319b987
muffin-rice <davidluzhu@gmail.com>
2021-07-15 17:04:40 -0700

    renamed file; readded job_generator

--- job_generator.py
+++ job_generator.py
@@ -1,25 +1,63 @@
 from job import Job
 import scipy.stats as ss
 import random
+import pandas as pd
+import numpy as np
+
+MAX_DURATION = 20000
+
+def transform_data(df : pd.DataFrame):
+    # data in duration, # gpus, # nodes
+    df = df.iloc[:,[3,4,5]]
+
+    df.drop(labels = df[df.iloc[:,0] > MAX_DURATION].index, inplace=True)
+    df.reset_index(inplace=True, drop=True)
+    df.iloc[:,0].clip(upper=MAX_DURATION, inplace = True)
+    #remove when # gpus or duration = 0
+    df.clip(lower = 1, inplace=True)
+    return df
 
 class Synthetic_Job_Generator:
     def __init__(self):
         self.internal_id = 1
-        pass
+        self.time_until_next_job = 0
+        self.reset_timer()
+        self.df = transform_data(pd.read_csv('data/pd_data.csv', sep=','))
+
+    def reset_timer(self):
+        self.time_until_next_job = int(ss.expon.rvs(scale=180))
 
     def create_job(self, timestamp):
 
-        times = ss.norm.rvs(loc = 250, scale = 100, size=2)
-        user_time, true_time = max(times[0], 10), max(times[1], 10)
+        #GPU asked, duration, and time between jobs actually somewhat independent; can naively model so
+
+        row = np.array(self.df.sample())[0] #duration, gpus, nodes
+        sample_duration, sample_gpu = int(row[0]/2), row[1]
+        rvs = [max(rv, 0) for rv in ss.norm.rvs(loc=1, scale=.15, size=2)]
+
+        true_time = sample_duration * rvs[0] #sample noise
+        user_time = true_time * rvs[1] #user estimate noise
 
         #[.25, .25, .25, .25, 0]
         #[.2, .1, .2, .25, .25]
-        gpu_req = random.choices([1,2,4,8,12], weights = [.15, .1, .1, .4, .25], k = 1)[0]
+
+        if random.random() < 1/4:
+            gpu_req = random.choices([1,2,4,8], weights = [.6,.15,.05,.3], k = 1)[0]
+        else:
+            gpu_req = sample_gpu
 
         new_job = Job(id=self.internal_id, start_time=timestamp, num_iterations=1000000,
                       user_time_estimates={gpu_req : user_time}, req_gpu=gpu_req,
                       true_time_rates={gpu_req : true_time})
 
         self.internal_id += 1
+        self.reset_timer()
 
         return new_job
+
+    @property
+    def job_arrived(self):
+        if self.time_until_next_job <= 0:
+            return True
+        else:
+            self.time_until_next_job -= 1
